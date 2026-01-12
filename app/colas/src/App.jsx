1687622b6ee76aad00f702c22b20fab4fd6b621f
Manuel Fabian <111201650+mc-stick@users.noreply.github.com>
2026-01-12 10:01:15 -0400

    new commit

---
+++ app/colas/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MenuInicial from './pages/MenuInicial';
+import PantallaAnuncios from './pages/PantallaAnuncios';
+import PantallaCliente from './pages/PantallaCliente';
+import PantallaAdmin from './pages/PantallaAdmin';
+import PantallaOperador from './pages/PantallaOperador';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MenuInicial />} />
+        <Route path="/anuncios" element={<PantallaAnuncios />} />
+        <Route path="/cliente" element={<PantallaCliente />} />
+        <Route path="/admin" element={<PantallaAdmin />} />
+        <Route path="/operador" element={<PantallaOperador />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
