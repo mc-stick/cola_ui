e2aaad74e7a9df5482617f6e35a47b4df3baa7a7
Manuel Fabian <111201650+mc-stick@users.noreply.github.com>
2026-02-11 15:20:15 -0400

    f

---
+++ server/Printservice.js
@@ -0,0 +1,85 @@
+import escpos from 'escpos';
+import USB from 'escpos-usb';
+const device = new USB();
+const options = { encoding: "CP850"}
+const printer = new escpos.Printer(device, options);
+import QRCode from 'qrcode';
+
+function fechaHoraActual12h() {
+  const d = new Date();
+
+  const dia = d.getDate().toString().padStart(2, '0');
+  const mes = (d.getMonth() + 1).toString().padStart(2, '0');
+  const anio = d.getFullYear();
+
+  let horas = d.getHours();
+  const minutos = d.getMinutes().toString().padStart(2, '0');
+  const ampm = horas >= 12 ? 'PM' : 'AM';
+
+  horas = horas % 12;
+  horas = horas ? horas : 12;
+
+  return `${dia}/${mes}/${anio} ${horas}:${minutos} ${ampm}`;
+}
+
+export async function imprimirTexto({ logo = "logo aqui", description = "", turno = "", servicio = '', footer = "footer" }) {
+
+  await new Promise((resolve, reject) => {
+    device.open(err => (err ? reject(err) : resolve()));
+  });
+
+
+let texto = `http://150.136.118.43/cola/evaluar/${turno.id}`
+
+  printer
+    .font('a')
+    .align('ct')
+    .size(2, 0.5)
+    .text(logo)
+    .size(0.5, 0.5)
+    .text(description)
+    .text('')
+    .text("Número de ticket")
+  printer
+    .font('B')
+    .size(2, 0.5)
+    .text('------------')
+    .text(turno.numero)
+    .text('------------')
+  printer
+    .font('a')
+    .size(1, 0.5)
+    .text(servicio)
+    .size(0.5, 0.5)
+    .text('\n')
+    .text('Espere al llamado de su turno.')
+
+
+  printer
+    .font('a')
+    .align('ct')
+    .size(0.5, 0.5)
+    .text(fechaHoraActual12h())
+
+printer
+    .font('a')
+    .size(0.5, 0.5)
+    .text('')
+    .text('Ayudanos a mejorar, califica nuestro servicio.')
+
+  printer
+    .qrimage(texto, {
+  type: 'png',
+  mode: 'dhdw',
+  size: 3 
+}, function(err){
+    this.cut();
+    this.text('');
+     this.close();
+  });
+
+
+}
+
+
+
